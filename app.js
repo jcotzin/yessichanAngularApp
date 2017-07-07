5d7d75a8c57ffddd434ba70675f0dde847e586f0
jcotzin <jcotzin@gmail.com>
2017-07-07 10:57:40 -0400

    added some route params for pages and api data

--- app.js
+++ app.js
@@ -3,16 +3,22 @@ var myApp = angular.module('myApp', ['ngRoute', 'ngResource']);
 myApp.config(function($routeProvider) {
     $routeProvider
     .when('/', {
-        templateUrl: 'pages/main.html',
+        templateUrl: 'pages/anime.html',
         controller: 'mainController'
     })
     .when('/blog', {
         templateUrl: 'pages/blog.html',
         controller: 'blogController'
     })
+    .when('/:pg', {
+        templateUrl: 'pages/anime.html',
+        controller: 'mainController'
+    })
 });
 
-myApp.controller('mainController', ['$scope', '$resource', function($scope, $resource) {
+myApp.controller('mainController', ['$scope', '$resource', '$routeParams', function($scope, $resource, $routeParams) {
+
+    $scope.pg = $routeParams.pg || '1';
 
     $scope.string = function(word) {
       return word.substring(0, 290);
@@ -25,9 +31,22 @@ myApp.controller('mainController', ['$scope', '$resource', function($scope, $res
       page3: "https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=40"
       };
 
-    $scope.animeApi = $resource($scope.pages.page1 +  'CLIENT_ID:%20dd031b32d2f56c990b1425efe6c42ad847e7fe3ab46bf1299f05ecd856bdb7dd&CLIENT_SECRET:%2054d7307928f63414defd96399fc31ba847961ceaecef3a5fd93144e960c0e151', {}, {}).get(function(response) {
-                            return response.data;
-                        });
+    $scope.idSecret = 'CLIENT_ID:%20dd031b32d2f56c990b1425efe6c42ad847e7fe3ab46bf1299f05ecd856bdb7dd&CLIENT_SECRET:%2054d7307928f63414defd96399fc31ba847961ceaecef3a5fd93144e960c0e151';
+
+    if ($scope.pg === '1') {
+        $scope.animeApi = $resource($scope.pages.page1 + $scope.idSecret, {}, {}).get(function(response) { return response.data; });
+    } else if ($scope.pg === '2') {
+        $scope.animeApi = $resource($scope.pages.page2 + $scope.idSecret, {}, {}).get(function(response) { return response.data; });
+    } else if ($scope.pg === '3'){
+        $scope.animeApi = $resource($scope.pages.page3 + $scope.idSecret, {}, {}).get(function(response) { return response.data; });
+    };
+
+
+
+
+
+
+
 
 }]);
 
